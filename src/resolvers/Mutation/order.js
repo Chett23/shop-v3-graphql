0c99bf1b02e015d0703a8ff3af5ac8dfc8c570a8
Chester Hansen <chester.hansen@pillpack.com>
2019-08-26 16:56:55 -0600

    changed order addToCart resolver slightly

--- src/resolvers/Mutation/order.js
+++ src/resolvers/Mutation/order.js
@@ -2,7 +2,7 @@ const order = {
   async addToCart(parent, {itemId, status, qty}, context) {
     return context.prisma.createItem({
         item: {connect: { id: itemId}},
-        status,
+        status: InCart,
         qty
     })
   },
